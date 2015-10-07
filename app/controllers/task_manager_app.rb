3ed51eb1b05bcabf7166130db2197b0f428646fd
Emily Dowdle <emily@editingemily.com>
2015-10-06 21:36:33 -0600

    working task manager app

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,29 @@
+require 'models/task_manager'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = TaskManager.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    TaskManager.create(params[:task])
+    redirect '/tasks'
+  end
+
+  get '/tasks/:id' do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :show
+  end
+
+end
